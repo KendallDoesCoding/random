9257bdb3edf4095d3bafd58bd3c69838fe7c8ba7
fk00750 <fk_00750@yahoo.com>
2022-08-05 00:39:58 +0530

    updating the javascript file

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 "use strict";
 
-function myFunction() {
+// Element
+const ReDirect_btn = document.getElementById("redirect__btn");
+
+// http Redirect 
+const myFunction = function () {
   location.replace("https://youtu.be/dQw4w9WgXcQ");
-}
+};
+
+// Listen to Click Event
+ReDirect_btn.addEventListener("click", (e) => {
+  myFunction();
+});
