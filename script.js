e0b17c03c4c4a547968f9f64a9e82116bafcf3d0
fk00750 <94318094+fk00750@users.noreply.github.com>
2022-08-05 13:21:59 +0530

    created a separate js file (#2)
    * created sepreate js file
    
    * updating the javascript file
    
    * refactoring

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Element
+const ReDirect_btn = document.getElementById("redirect__btn");
+
+// http Redirect 
+const myFunction = function () {
+  location.replace("https://youtu.be/dQw4w9WgXcQ");
+};
+
+// Listen to Click Event
+ReDirect_btn.addEventListener("click", () => {
+  myFunction();
+});
